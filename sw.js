6f9881a543606b3a5197fde463123495704fc1f9
aakash aggarwal <aakashaggarwal@aakashs-MacBook-Air.local>
2017-07-13 14:51:27 +0530

    Facebook

--- sw.js
+++ sw.js
@@ -40,6 +40,6 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow('https://developers.google.com/web/')
+    clients.openWindow('https://facebook.com')
   );
 });
